29ce4e424b61d993037e1e1ad8ed4c677d3258af
Evan Sonderegger <evan@rpy.xyz>
2020-04-09 09:16:16 -0400

    use md files with front-matter

--- docs_templates/Simple.jsx
+++ docs_templates/Simple.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default props => (
-  <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <title>{props.title}</title>
-      <link rel="canonical" href={props.url} />
-      <link rel="icon" href="/favicon.png" />
-      <meta name="description" content={props.description} />
-      <meta name="robots" content="index, follow" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link href={props.css_file} rel="stylesheet" />
-    </head>
-    <body>
-      <header>
-        <h1>{props.title}</h1>
-        <h2>{props.description}</h2>
-      </header>
-      <main dangerouslySetInnerHTML={{ __html: props.content }} />
-    </body>
-  </html>
-);
+const simple = (props) => {
+  const {
+    title, url, description, cssFile, content
+  } = props;
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <link rel="canonical" href={url} />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content={description} />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href={cssFile} rel="stylesheet" />
+      </head>
+      <body>
+        <header>
+          <h1>{title}</h1>
+          <h2>{description}</h2>
+        </header>
+        <main dangerouslySetInnerHTML={{ __html: content }} />
+      </body>
+    </html>
+  );
+};
+
+simple.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  cssFile: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default simple;
